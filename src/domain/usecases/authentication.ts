fa77b6884e488feb7e024b2a4b7a5c880c2012f9
Jonas Tolentino <jonasmarco.ti@gmail.com>
2022-01-24 21:45:51 -0300

    refactor: move HttpPostClientSpy to its own file

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,4 +1,4 @@
-import { AccountModel } '../models/account-model'
+import { AccountModel } from '../models/account-model'
 
 type AuthenticationParams = {
   email: string
@@ -6,5 +6,5 @@ type AuthenticationParams = {
 }
 
 export interface Authentication {
-  auth (params: AuthenticationParams): Promise<AccountModel>
+  auth(params: AuthenticationParams): Promise<AccountModel>
 }
